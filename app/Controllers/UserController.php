0de7b4f4fa3d5d489ed995da3c30d3b675474492
Rudy Artha <rudy.artha@guestpro.id>
2023-05-22 22:31:11 +0800

    add cors headers

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -10,6 +10,10 @@ class UserController extends BaseController
 
     public function index()
     {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
+        header('Access-Control-Allow-Headers: Content-Type, Authorization');
+        
         $userModel = new UserModel();        
         $data['success'] = True;
         $data['data'] = $userModel->orderBy('id', 'DESC')->findAll();
